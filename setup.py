ee1f4f84c0faad35ebb4c7a87b56295d1af70411
Derek Dalle <derek.j.dalle@nasa.gov>
2021-06-08 15:43:05 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Standard library
+from setuptools import setup
+
+
+# Create the build
+setup(
+    name="slsconst",
+    packages=[
+        "slsconst"
+    ],
+    description="Reference constants for Space Launch System aerodynamics",
+    version="1.0")
+
