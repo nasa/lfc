e9bb489c1ff4071bcde98079f7a814261f9e06af
Derek J. Dalle <derek.j.dalle@nasa.gov>
2021-11-17 15:08:12 -0800

    Genericize template package name

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # Standard library
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 # Create the build
 setup(
-    name="tnas49",
-    packages=[
-        "tnas49"
-    ],
+    name="mypkg",
+    packages=find_packages(),
     description="Template repo for Python packages",
     version="1.0")
 
