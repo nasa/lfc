e6158693476a1a6cbbc272659845dbf9d46f2d56
Derek Dalle <derek.j.dalle@nasa.gov>
2021-12-03 23:25:18 -0800

    Fix one name in test template command

--- run_test_py3.sh
+++ run_test_py3.sh
@@ -3,4 +3,5 @@
 python3 -m pytest \
     --junitxml=test/junit.xml \
     --report-log=test/log.json \
-    --cov=argread --cov-report html:doc/_build/html/cov
+    --cov=mypkg \
+    --cov-report html:doc/_build/html/cov
