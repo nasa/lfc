a593e9355f8d4a498690fef5b92d9a90367e3579
Derek Dalle <derek.j.dalle@nasa.gov>
2021-12-28 11:39:38 -0800

    Update test coverage template

--- run_test_py3.sh
+++ run_test_py3.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+PKG=$(python3 -c "from setuptools import find_packages
+print(find_packages()[0])")
+
 python3 -m pytest \
+    --pdb \
     --junitxml=test/junit.xml \
     --report-log=test/log.json \
-    --cov=mypkg \
-    --cov-report html:doc/_build/html/cov
+    --cov=$PKG \
+    --cov-report html:test/htmlcov 
