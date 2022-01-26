e0cec8bd62b1541197fda3f444cab8e85bd2bad1
Derek Dalle <derek.j.dalle@nasa.gov>
2022-01-26 10:50:23 -0800

    Modify template command to execute tests

--- run_test_py3.sh
+++ run_test_py3.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Get package name (type manually if necessary)
 PKG=$(python3 -c "from setuptools import find_packages
 print(find_packages()[0])")
 
+# Run tests
 python3 -m pytest \
     --pdb \
-    --junitxml=test/junit.xml \
     --report-log=test/log.json \
     --cov=$PKG \
     --cov-report html:test/htmlcov 
+
+# Clear out .gitignore for coverage report
+rm test/htmlcov/.gitignore
+
