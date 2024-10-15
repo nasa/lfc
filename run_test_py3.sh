90e24f86bc69c9ceb13c718d4150424f0562399c
Derek Dalle <derek.j.dalle@nasa.gov>
2024-10-14 22:54:23 -0700

    Mess with coverage settings to ignore clidoc/ folder

--- run_test_py3.sh
+++ run_test_py3.sh
@@ -4,7 +4,9 @@
 PKG=lfc
 
 # Run tests
-python3 -m pytest \
+python3 -m coverage run \
+    --omit="lfc/clidoc/*.py" \
+    -m pytest \
     --junitxml=test/junit.xml \
     --cov=$PKG \
     --pdb \
